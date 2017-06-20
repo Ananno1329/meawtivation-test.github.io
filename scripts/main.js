bb43c6815c5af665105919e0c149c1d6ee00e09c
Ananno1329 <ananno.31@gmail.com>
2017-06-20 13:25:36 +0600

    First Update

---
+++ scripts/main.js
@@ -0,0 +1,29 @@
+(function($) {
+
+	// Document ready
+	$(document).ready(function() {
+
+		// Fancybox initialization
+		$('.gallery-image').fancybox();
+
+		// Navigation click functionality
+		$('.navigation a').on('click', function() {
+			var _this = $(this),
+					target = _this.data('target');
+
+			_this.parent().addClass('active').siblings().removeClass('active');
+
+			scrollTo(target);
+
+			return false;
+		});
+	});
+
+	// Scroll to section
+	function scrollTo(element) {
+		$('html, body').animate({
+			scrollTop: $('#' + element).offset().top - 100
+		});
+	}
+
+})(jQuery);
